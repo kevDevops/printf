a4b4209d0962743c2933069dbbf52303072024a3
kevDevops <kelvinnyagaka7@gmail.com>
2023-11-14 17:36:19 -0800

    reach

--- functions2.c
+++ functions2.c
@@ -95,7 +95,7 @@ int print_non_printable(va_list types, char buffer[],
 /************************* PRINT REVERSE *************************/
 /**
  * print_reverse - Prints reverse string.
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
